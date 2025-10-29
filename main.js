9357888fe6819a6ab7afe9ebd15fab326c8c8a4f
Jordana666 <jordana.e.silva@escola.pr.gov.br>
2025-10-29 07:57:29 -0300

    .

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ console.log(avanca)
 
 avanca.forEach(button =>{
   button.addEventListener('click', function(){
-   const atual = document.querySelector('.ativo')
-   const proximoPasso = 'passo-' + getAttribute('data-proximo')
+    const atual = document.querySelector('.ativo')
+    const proximoPasso = 'passo-' + getAttribute('data-proximo')
 
-   atual.classList.remove('ativo')
-   document.getElementById(proximoPasso).classList.add('ativo')
+    atual.classList.remove('ativo')
+    document.getElementById(proximoPasso).classList.add('ativo')
  })
 })
